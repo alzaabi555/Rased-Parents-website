39fdfb9ce1cc3c88a6f32639ab48310bdb3404d4
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-10 22:34:11 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // اكتب هنا اسم مستودع الموقع الجديد الذي أنشأته على جيت هب (بين علامتي / /)
+  plugins: [react(), tailwindcss()],
+  // تأكد أن هذا الاسم يطابق اسم مستودعك الجديد على جيت هب
   base: '/Rased-Parents-website/', 
 })
